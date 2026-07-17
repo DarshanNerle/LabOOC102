5e420a66c8a444151d2782b74e2d82c8424d5d46
Darshan Dattatray Nerle <darshannerle13101987@gmail.com>
2026-07-17 12:36:43 +0000

    sum of two numbwrs

---
+++ sum_of_two_num.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    cout<<"Enter the a : ";
+    cin>> a;
+
+    cout<<"Enter the b : ";
+    cin>> b ;
+
+    c = a + b;
+
+    cout<<"Sum of a & b is : " << c <<"\n";
+    
+    return 0;
+}
