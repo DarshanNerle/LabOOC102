3a4fa8e27b41cb3ced2239c1117f5f96d8d096e1
Darshan Dattatray Nerle <darshannerle13101987@gmail.com>
2026-07-30 11:02:20 +0000

    Add C++ program for dynamic memory allocation and deallocation of an integer array

---
+++ Pointer.cpp/Memory.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *arr;
+    int size;
+
+    cout<<"Enter the size of the integer array:";
+    cin>>size;
+
+    cout<<"Creating an array of size" <<size <<"..";
+    arr = new int[size];
+
+    cout<<"\n Dynamic allocation of memory for array arr is successful." <<endl;
+
+    delete arr;
+
+    return 0;
+}
